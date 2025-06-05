e480148c53ac312f6474a9df1eef889ccf889e6a
nakulb23 <154494941+nakulb23@users.noreply.github.com>
2025-06-05 10:08:12 -0700

    Initial Commit

---
+++ Golf Swing AI/Golf_Swing_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Golf_Swing_AIApp.swift
+//  Golf Swing AI
+//
+//  Created by Nakul Bhatnagar on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Golf_Swing_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Golf Swing AITests/Golf_Swing_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  Golf_Swing_AITests.swift
+//  Golf Swing AITests
+//
+//  Created by Nakul Bhatnagar on 6/5/25.
+//
+
+import Testing
+@testable import Golf_Swing_AI
+
+struct Golf_Swing_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
